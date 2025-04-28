bed4034f2f5b11bdb6c3bc51cfcbea28a045d9cb
rootlinux777 <gsvmch777@gmail.com>
2025-04-28 23:42:00 +0600

    Create app.js and here Vscode basic instruction

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-
+/**
+ * VsCode:
+ * 
+ * => download vscode application by using browser.
+ * => install vscode application in your windows.
+ * => ctrl + shift + p = command terminal
+ * => install font firacode and setting fontLigatures: true
+ * => install extension: material icon, quokkajs, pettier, liveserver, tailwindcss intelecense.
+ * => ctrl + p, ctrl + tab, ctrl + r, ctrl + o, ctrl + d, ctrl + shift + d, 
+ *    ctrl + `, startup editor = none, font-family: firacode, wordwrap: true, 
+ *    autosave: afterDealy.
+ * => etc.
+ */
